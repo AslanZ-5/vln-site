229d51774ac75e3f0907996f19b8a0a745890c93
Зелимхан Пашаев <zelimkhan.pashaev@ic-group.ru>
2023-09-13 11:12:14 +0000

    Features/carousel

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/widgets/headeer";
 import { ReactNode } from "react";
-import { Slider } from "@/widgets/carousel/ui/slider";
 import { Sidebar } from "@/widgets/sidebar";
+
 import styles from "./layout.module.css";
 
 export function Layout({ children }: { children: ReactNode }) {

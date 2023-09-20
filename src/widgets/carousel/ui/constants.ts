0b14378cecbcdbd352a63cdd4ae2c2f6ca57f218
Зелимхан Пашаев <zelimkhan.pashaev@ic-group.ru>
2023-09-20 13:40:37 +0000

    Feature/slider

---
+++ src/widgets/carousel/ui/constants.ts
@@ -0,0 +1,19 @@
+export const slidesMock = [
+  {
+    path: "/img/image1.jpg",
+    alt: "slide1",
+  },
+  {
+    path: "/img/image1.jpg",
+    alt: "slide2",
+  },
+  {
+    path: "/img/image1.jpg",
+    alt: "slide3",
+  },
+];
+
+export type Slide = {
+  path: string;
+  alt: string;
+};

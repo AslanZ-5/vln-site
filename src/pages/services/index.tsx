19b970548ab7111b9ae3c634df7f16180b69435f
Омиргалиева Азиза <aziza.omirgaliyeva@ic-group.ru>
2023-10-04 12:26:59 +0000

    Feature/services page layout

---
+++ src/pages/services/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './services-page.module.scss';
+
+function ServicesPage() {
+  return (
+    <div className={styles.wrapper}>
+      <div>bread crumbs</div>
+      <div className={styles.container}>
+        <div>title</div>
+        <div>cards</div>
+      </div>
+    </div>
+  );
+}
+
+export default ServicesPage;

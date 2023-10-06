2422994de1aa5c0bf57e59fabb5ea95e87289f88
Глоба Даниил Викторович <daniil.globa@volnamobile.ru>
2023-10-06 11:51:54 +0000

    Feature/location

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -4,3 +4,4 @@ export { Button } from './button/button';
 export { CurrencyInput } from './currency-input/currency-input';
 export { PhoneInput } from './phone-input/phone-input';
 export { Breadcrumbs } from './breadcrumbs/breadcrumbs';
+export { Link } from './link/link';

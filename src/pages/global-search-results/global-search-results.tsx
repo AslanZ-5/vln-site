e8b7a944c38496a18502e84a85e4cd33e940993b
Рахмаев Умар Мовсарович <umar.rakhmaev@ic-group.ru>
2023-10-06 13:21:08 +0000

    feature/VOLNA68: Результаты поиска

---
+++ src/pages/global-search-results/global-search-results.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./global-search-results.module.scss";
+import { Breadcrumbs } from "@/shared/ui-kit";
+import { breadCrumbsItems, questions } from "./global-search-results.constants";
+
+const resultsCount = 187;
+
+export default function GlobalSearchResults() {
+  return (
+    <div className={styles.wrapper}>
+      <Breadcrumbs list={breadCrumbsItems} />
+      <div
+        className={styles.title}
+      >{`Результаты поиска (${resultsCount})`}</div>
+      <div className={styles.questions}>
+        {questions.map((question, index) => {
+          return (
+            <div className={styles.questWrapper} key={index}>
+              <div className={styles.questTitle}>{question.question}</div>
+              <div className={styles.answer}>{question.answer}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}

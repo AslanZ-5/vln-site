2bf7ddb3daefb2f62885805eb1d25711e1168e89
Александр Владимирович Димов <aleksandr.dimov@ic-group.ru>
2023-10-06 09:51:48 +0000

    Frequently Asked Questions page

---
+++ src/modules/pages/faq/cards.tsx
@@ -0,0 +1,44 @@
+import { Card } from '@mantine/core';
+import styles from './faq-page.module.scss';
+import { volnaTheme as theme } from '@/shared/constants/theme';
+import { ChevronRight } from '@/shared/assets/svg';
+import { CARD_CONTENTS } from './constants';
+
+function Cards() {
+  return (
+    <>
+      { CARD_CONTENTS.map((content, i) => {
+          return (
+            <Card
+              key={i}
+              className={styles.card}
+              radius={16}
+              padding={'0 0 32px 0'}
+              withBorder
+            >
+              <Card.Section
+                className={styles.cardHeader}
+                style={{ backgroundColor: theme.colors.primary[100] }}
+              >
+                <div>
+                  <div className={styles.headerTitle}>
+                    { content.header.icon }
+                    <span>{ content.header.title }</span>
+                  </div>
+                  <div className={styles.headerArrowIcon}>
+                    <ChevronRight />
+                  </div>
+                </div>
+              </Card.Section>
+              <Card.Section className={styles.cardContent}>
+                { content.text }
+              </Card.Section>
+            </Card>
+          )
+        }
+      )
+    }
+  </>
+)}
+
+export default Cards;

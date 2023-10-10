b06d20ddd8ea017b358eb348d38a504c5a0f7715
@aziza.omirgaliyeva <aziza.omirgaliyeva@ic-group.ru>
2023-10-10 14:31:56 +0600

    add mantine phone input

--- src/shared/ui-kit/phone-input/phone-input.tsx
+++ src/shared/ui-kit/phone-input/phone-input.tsx
@@ -1,109 +1,96 @@
-import { ChangeEvent, FC, MutableRefObject, useEffect, useId, useState } from 'react';
-import { PhoneInputProps } from './phone-input.types';
+import React, { FC, useState } from 'react';
 import { CloseBtn } from '@/shared/assets/icons';
-import { useIMask } from 'react-imask';
 import { volnaTheme as theme } from '@/shared/constants/theme';
 import cn from 'classnames';
 import styles from './phone-input.module.scss';
+import { Input, InputProps } from '@mantine/core';
+import { IMaskInput } from 'react-imask';
+
+interface PhoneInputProps extends InputProps {
+  onChange?: (value: string) => void;
+  darkBackground?: boolean;
+  label?: string;
+  mask?: string;
+  placeholder?: string;
+  value?: string;
+}
 
 export const PhoneInput: FC<PhoneInputProps> = ({
-  changeHandler,
-  clearHandler,
-  className,
-  containerWidth,
+  onChange: changeHandler,
   darkBackground,
-  errorMessage,
-  id,
-  isError,
-  isReady,
+  error,
+  disabled,
   label,
   mask,
-  name,
   placeholder = '+7 (___) ___ - __ - __',
-  ref: nativeRef,
   value: initialValue,
+  className,
+  ...props
 }) => {
-  const { ref, value, unmaskedValue, setValue } = useIMask({
-    mask: [
-      {
-        mask: '',
-      },
-      {
-        mask: mask || '+7 (000) 000 - 00 - 00',
-        lazy: false,
-        placeholderChar: '_',
-      },
-    ],
-  });
-
+  const [value, setValue] = useState(initialValue || '');
   const [isActive, setIsActive] = useState(false);
-  const inputId = useId();
 
-  const installRefs = (instance: HTMLInputElement) => {
-    (ref as MutableRefObject<HTMLInputElement>).current = instance;
-
-    if (nativeRef) {
-      (nativeRef as MutableRefObject<HTMLInputElement>).current = instance;
-    }
-  };
-
-  useEffect(() => {
-    if (initialValue || initialValue === '') {
-      setValue && setValue(initialValue);
-    }
-  }, [initialValue, setValue]);
-
-  const onClearInput = () => {
-    setValue && setValue('');
-    clearHandler && clearHandler();
-  };
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    changeHandler && changeHandler(value, unmaskedValue, event);
-    unmaskedValue.length === 10 && isReady && isReady(unmaskedValue);
+  const onChange = (val: string) => {
+    changeHandler && changeHandler(val);
+    setValue(val);
   };
 
   return (
-    <div
-      className={cn(styles.container, darkBackground && styles.dark, className && className)}
-      style={{ width: containerWidth }}>
-      {label && (
-        <label className={styles.label} htmlFor={id || inputId}>
-          {label}
-        </label>
-      )}
-      <div
-        className={cn(styles.inputWrapper, isActive && styles.isActive, isError && styles.isError)}>
-        <input
-          className={cn(styles.input, isError && styles.invalid)}
-          value={value}
-          ref={installRefs}
-          onInput={onChange}
-          autoComplete="off"
-          placeholder={placeholder}
-          name={name}
-          id={id || inputId}
-          onFocus={() => setIsActive(true)}
-          onBlur={() => setIsActive(false)}
-        />
-        <button
-          type="button"
-          onClick={onClearInput}
-          className={cn(styles.closeBtn, value && isActive && styles.visible)}>
-          <CloseBtn
-            color={
-              isError
-                ? darkBackground
-                  ? theme.colors.option.dance
-                  : theme.colors.error.fire
-                : darkBackground
-                ? theme.colors.base[200]
-                : theme.colors.base[400]
-            }
-          />
-        </button>
-      </div>
-      {isError && errorMessage && <div className={styles.errorMessage}>{errorMessage}</div>}
-    </div>
+    <Input.Wrapper
+      classNames={{
+        root: cn(styles.wrapper__root, darkBackground && styles.dark, className && className),
+        label: styles.wrapper__label,
+        error: styles.wrapper__error,
+      }}
+      error={error}
+      label={label}
+    >
+      <Input
+        classNames={{
+          input: styles.input__input,
+          rightSection: styles.input__right_section,
+          wrapper: styles.input__wrapper,
+        }}
+        disabled={disabled}
+        error={error}
+        value={value}
+        placeholder={placeholder}
+        onInput={(event) => onChange(event.currentTarget.value)}
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(false)}
+        component={IMaskInput}
+        mask={[
+          {
+            mask: '',
+          },
+          {
+            mask: mask || '+7 (000) 000 - 00 - 00',
+            lazy: false,
+            placeholderChar: '_',
+          },
+        ]}
+        rightSectionWidth={60}
+        rightSection={
+          <button
+            type='button'
+            onClick={() => onChange('')}
+            className={cn(styles.closeBtn, value && isActive && styles.visible)}
+          >
+            <CloseBtn
+              color={
+                error
+                  ? darkBackground
+                    ? theme.colors.option.dance
+                    : theme.colors.error.fire
+                  : darkBackground
+                  ? theme.colors.base[200]
+                  : theme.colors.base[400]
+              }
+            />
+          </button>
+        }
+        {...props}
+      />
+    </Input.Wrapper>
   );
 };

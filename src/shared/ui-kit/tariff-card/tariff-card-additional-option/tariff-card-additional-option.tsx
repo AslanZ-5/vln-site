e2e2ea56bb0ebd3b1141dbe2528dde4702aed5a8
Daniil Globa <Daniil.Globa@ic-group.ru>
2023-10-10 15:33:29 +0300

    remove redundant file

--- src/shared/ui-kit/tariff-card/tariff-card-additional-option/tariff-card-additional-option.tsx
+++ src/shared/ui-kit/tariff-card/tariff-card-additional-option/tariff-card-additional-option.tsx
@@ -19,7 +19,7 @@ const TariffCardAdditionalOption: FC<TariffCardAdditionalProps> = ({
         </div>
         <div className={styles.discountPrice}>-{discount}%</div>
       </div>
-      <Switch size={SwitchSize.LARGE} onChange={onChangeSwith} size={SwitchSize.LARGE} />
+      <Switch size={SwitchSize.LARGE} onChange={onChangeSwith} />
     </div>
   );
 };
